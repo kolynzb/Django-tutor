46d3536c9870453313b31bb2468da1acb88ca611
kolynzb <collinsbenda360@gmail.com>
2022-05-10 21:33:45 +0300

    Edit Admin Page

--- store/admin.py
+++ store/admin.py
@@ -3,7 +3,14 @@ from . import models
 
 
 # Customise list pages 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','unit_price']
+    list_editable = ['unit_price'] #allows editing on a particularfeild 
+    list_per_page = 10 #Ten items per page
+    # Google django model admin options
+# admin.site.register(models.Product,ProductAdmin)
+
 
 # Register your models here.
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
